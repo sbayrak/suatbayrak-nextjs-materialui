44c87f7833365be8d776e56b02aad3cf15d1f15f
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-06-04 11:39:56 +0300

    added imgs and img links for projects

--- src/projects.js
+++ src/projects.js
@@ -5,7 +5,7 @@ export const projects = [
     desc: 'SB Steakhouse template website for restaurants.',
     website: 'https://bayrak-steakhouse.netlify.app/',
     repository: 'https://github.com/sbayrak/sb-steak-house',
-    img: '',
+    img: '/react_logo.png',
   },
   {
     id: '2',
@@ -14,7 +14,7 @@ export const projects = [
       'SBShop is a online shop sells electronics. Only PayPal payment is accepted. Stripe will come soon!. Also working on accepting crypto payments...',
     website: 'https://sbshop.herokuapp.com/',
     repository: 'https://github.com/sbayrak/sbshop-mern',
-    img: '',
+    img: '/mern_logo.png',
   },
   {
     id: '3',
@@ -23,7 +23,7 @@ export const projects = [
       'Used BreakingBad api to display characters of the show. Also has a search bar.',
     website: 'https://breakingbad01.netlify.app/',
     repository: 'https://github.com/sbayrak/breakingbad-api-react',
-    img: '',
+    img: '/react_logo.png',
   },
   {
     id: '4',
@@ -31,7 +31,7 @@ export const projects = [
     desc: 'Simple contact keeper app on web.',
     website: '',
     repository: 'https://github.com/sbayrak/mern-contactkeeper',
-    img: '',
+    img: '/mern_logo.png',
   },
   {
     id: '5',
@@ -40,7 +40,7 @@ export const projects = [
       'Medium size social media for developers, where you can create a profile, education information, make posts, comment and like functionality.',
     website: 'https://devconnector52.herokuapp.com/',
     repository: 'https://github.com/sbayrak/mern-devconnector',
-    img: '',
+    img: '/mern_logo.png',
   },
   {
     id: '6',
@@ -49,7 +49,7 @@ export const projects = [
       'AuctionHouse is a tender based web application where the companies may register with requested mandatory informations (tax number, website, tax office name, etc...) and open a tender and get offers from other companies.',
     website: 'https://auctionhouse12.herokuapp.com/',
     repository: 'https://github.com/sbayrak/auctionhouse',
-    img: '',
+    img: '/mern_logo.png',
   },
   {
     id: '7',
@@ -57,7 +57,7 @@ export const projects = [
     desc: 'Cryptocurrency market made with API from coingecko and ReactJS.',
     website: 'https://cryptomarket.netlify.app/',
     repository: 'https://github.com/sbayrak/react-cryptocurrency-api',
-    img: '',
+    img: '/react_logo.png',
   },
   {
     id: '7',
@@ -66,7 +66,7 @@ export const projects = [
       'A fan of Pokemon ? You may find your favorite pokemons in this website among thousands of pokemons...',
     website: 'https://react-pokemonapi.web.app/',
     repository: 'https://github.com/sbayrak/react-pokemonapi',
-    img: '',
+    img: '/react_logo.png',
   },
   {
     id: '8',
@@ -74,6 +74,6 @@ export const projects = [
     desc: 'Facebook Messenger clone with ReactJS and Firebase.',
     website: 'https://fbmessengerclone-7bc8a.web.app/',
     repository: 'https://github.com/sbayrak/fbmessengerclone',
-    img: '',
+    img: '/react_logo.png',
   },
 ];
