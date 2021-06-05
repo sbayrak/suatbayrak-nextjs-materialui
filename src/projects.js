fbe7fa64c4cf65165c90822e45a117c5bd89313a
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-06-05 18:03:13 +0300

    added website imgs to project cards

--- src/projects.js
+++ src/projects.js
@@ -5,7 +5,7 @@ export const projects = [
     desc: 'SB Steakhouse template website for restaurants.',
     website: 'https://bayrak-steakhouse.netlify.app/',
     repository: 'https://github.com/sbayrak/sb-steak-house',
-    img: '/react_logo.png',
+    img: '/portfolio1.png',
   },
   {
     id: '2',
@@ -14,7 +14,7 @@ export const projects = [
       'SBShop is a online shop sells electronics. Only PayPal payment is accepted. Stripe will come soon!. Also working on accepting crypto payments...',
     website: 'https://sbshop.herokuapp.com/',
     repository: 'https://github.com/sbayrak/sbshop-mern',
-    img: '/mern_logo.png',
+    img: '/portfolio2.png',
   },
   {
     id: '3',
@@ -31,7 +31,7 @@ export const projects = [
     desc: 'Simple contact keeper app on web.',
     website: '',
     repository: 'https://github.com/sbayrak/mern-contactkeeper',
-    img: '/mern_logo.png',
+    img: '/portfolio3.png',
   },
   {
     id: '5',
@@ -40,7 +40,7 @@ export const projects = [
       'Medium size social media for developers, where you can create a profile, education information, make posts, comment and like functionality.',
     website: 'https://devconnector52.herokuapp.com/',
     repository: 'https://github.com/sbayrak/mern-devconnector',
-    img: '/mern_logo.png',
+    img: '/portfolio4.png',
   },
   {
     id: '6',
@@ -49,7 +49,7 @@ export const projects = [
       'AuctionHouse is a tender based web application where the companies may register with requested mandatory informations (tax number, website, tax office name, etc...) and open a tender and get offers from other companies.',
     website: 'https://auctionhouse12.herokuapp.com/',
     repository: 'https://github.com/sbayrak/auctionhouse',
-    img: '/mern_logo.png',
+    img: '/portfolio5.png',
   },
   {
     id: '7',
@@ -57,7 +57,7 @@ export const projects = [
     desc: 'Cryptocurrency market made with API from coingecko and ReactJS.',
     website: 'https://cryptomarket.netlify.app/',
     repository: 'https://github.com/sbayrak/react-cryptocurrency-api',
-    img: '/react_logo.png',
+    img: '/portfolio6.png',
   },
   {
     id: '7',
@@ -66,7 +66,7 @@ export const projects = [
       'A fan of Pokemon ? You may find your favorite pokemons in this website among thousands of pokemons...',
     website: 'https://react-pokemonapi.web.app/',
     repository: 'https://github.com/sbayrak/react-pokemonapi',
-    img: '/react_logo.png',
+    img: '/portfolio7.png',
   },
   {
     id: '8',
