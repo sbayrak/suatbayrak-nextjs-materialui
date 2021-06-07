1015e9bd28128ae255f844d120a13e0a2f0a134f
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-06-07 11:52:05 +0300

    added google tag manager scripts back

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          {/* <script
+          <script
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -20,7 +20,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           <meta
             name='google-site-verification'
             content='G19UOSOLedVnIhpeey7SJ4mtTO6fALqUvSxsZZ4KxLo'
-          /> */}
+          />
           {/* PWA primary color */}
           <meta name='theme-color' content={theme.palette.primary.main} />
           <link
@@ -45,14 +45,12 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             fontFamily: 'Montserrat',
           }}
         >
-          {/* <noscript>
-            <iframe
-              src='https://www.googletagmanager.com/ns.html?id=GTM-TCCGQNQ'
-              height='0'
-              width='0'
-              style='display:none;visibility:hidden'
-            ></iframe>
-          </noscript> */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TCCGQNQ"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
           <Main />
           <NextScript />
         </body>
