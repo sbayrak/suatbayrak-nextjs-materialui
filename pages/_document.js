e55317bf29ff9720fb32b2759816b250ebce8b73
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-06-07 11:40:05 +0300

    trying to deploy

--- pages/_document.js
+++ pages/_document.js
@@ -45,12 +45,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             fontFamily: 'Montserrat',
           }}
         >
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TCCGQNQ"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          ></noscript>
           <noscript>
             <iframe
               src='https://www.googletagmanager.com/ns.html?id=GTM-TCCGQNQ'
