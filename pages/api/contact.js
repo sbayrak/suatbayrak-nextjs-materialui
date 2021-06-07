d83d03764d5ad10c3a7f070fb4ca81e101328c35
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-06-07 10:34:23 +0300

    edited contact form api

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,11 +2,20 @@ import sgMail from '@sendgrid/mail';
 
 export default async (req, res) => {
   sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
+
+  const { name, email, message } = req.body;
+
+  let userForm = {
+    name: name,
+    email: email,
+    message: message,
+  };
   const msg = {
     to: 'suat.bayrak@bilgiedu.net', // Change to your recipient
-    from: req.body.email, // Change to your verified sender
-    subject: `Website Contact - ${req.body.name}`,
-    text: req.body.message,
+    from: 'suat.bayrak@bilgiedu.net', // Change to your verified sender
+    subject: `suatbayrak.com Website Contact - ${req.body.name}`,
+    text: JSON.stringify(userForm),
+    html: `<h3>Name : ${name} <br/> E-Mail : ${email} <br/> Message : ${message} </h3> <br/> <h1>This e-mail has been sent from suatbayrak.com contact form. <br/> </h1> `,
   };
   sgMail
     .send(msg)
